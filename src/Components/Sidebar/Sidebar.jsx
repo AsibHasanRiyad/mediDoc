8432f0e954141fe0d1067359a5c66b71dfe56af5
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-12-19 01:15:26 +0600

    nav added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import { IoIosArrowBack } from "react-icons/io";
 import { FiMenu, FiHome, FiUser, FiCalendar } from "react-icons/fi";
 import { RiSettings4Line } from "react-icons/ri";
 import { MdHistory } from "react-icons/md";
+
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   const iconData = [
@@ -16,7 +17,13 @@ const Sidebar = () => {
   return (
     <div className=" flex">
       <div
-        className={`bg-white w-60 min-h-screen relative  duration-300 ${
+        style={{
+          position:
+            (open && window.innerWidth <= 640) || window.innerWidth <= 834
+              ? "fixed"
+              : "relative",
+        }}
+        className={`bg-white min-h-screen relative  duration-300 ${
           open ? "w-[270px]" : " w-[70px] "
         }`}
       >
@@ -78,78 +85,69 @@ const Sidebar = () => {
           </div>
         )}
       </div>
-      <div className=" bg-[#F9F9F9] w-screen p-8 pt-5  text-justify ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nihil velit
-        dolor iure aliquid earum possimus exercitationem odit, beatae veniam
-        quod dolores molestiae deleniti architecto nostrum, temporibus tenetur
-        nobis asperiores eum delectus dolorum dolore dolorem? Deleniti
-        laudantium inventore non cumque in doloribus aliquam, fugiat quis
-        voluptas, sit tempore rerum sunt expedita quae nesciunt. Quisquam vitae
-        molestiae eum mollitia debitis esse?Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Maiores non sit consectetur nobis unde
-        obcaecati inventore deserunt dolore, animi veritatis et! Provident nemo
-        corrupti temporibus aliquam neque laborum optio, rerum exercitationem
-        aliquid deleniti nobis magni fugiat officia fuga deserunt dolore ut?
-        Deserunt porro cumque laboriosam optio sit esse iste rerum iusto debitis
-        dicta molestias incidunt, nesciunt unde earum fugiat. Quo quod,
-        veritatis cum illo explicabo maxime id? Repellat ab animi quia nisi
-        porro sit facilis asperiores doloremque, rerum, libero nostrum commodi
-        unde pariatur maxime? Quae, ex porro dicta voluptate fugiat dignissimos
-        ab quisquam itaque placeat iusto! Iusto inventore id incidunt! Ex
-        dolorum quod odit sunt et mollitia ea non odio. Eius numquam nulla vel
-        nostrum sit odit voluptatem voluptatibus delectus corrupti consectetur,
-        odio voluptate sunt eum! Eos quisquam impedit quidem aperiam voluptatem
-        eum placeat officia quia, voluptas sint nihil omnis repellendus
-        veritatis, deleniti incidunt sequi? Provident laborum eos reiciendis,
-        perferendis iste fugiat repellendus doloribus ex, impedit similique odit
-        accusamus, accusantium velit sapiente voluptas nostrum possimus
-        aspernatur aliquid? Repudiandae possimus dignissimos incidunt magnam
-        perferendis quis ad ducimus obcaecati tempore cumque! Rerum quod aliquid
-        quaerat aperiam error delectus facilis ullam non laboriosam. Cum rem
-        dolores commodi? Aspernatur optio ad quae explicabo dignissimos
-        asperiores minus. Illum earum minima animi velit, labore adipisci id
-        repellendus explicabo necessitatibus dolorum commodi autem reiciendis!
-        Ducimus nulla dolorem totam maiores quasi odit! Cupiditate aut eaque
-        dolor laborum nisi quidem a quisquam fugit esse repudiandae ad ipsa,
-        neque quibusdam autem dolores, soluta ex deserunt, consequuntur veniam!
-        Temporibus, vero? Mollitia tempora, eaque non quo corrupti iste rem
-        repellat, vero nobis architecto ipsam veniam enim dolor voluptatum.
-        Ratione tenetur, et suscipit, exercitationem eos excepturi blanditiis
-        nesciunt hic, atque quas praesentium inventore perspiciatis maiores esse
-        quae? Earum, odit fugit dolores exercitationem harum rerum soluta
-        eveniet doloremque perspiciatis sapiente obcaecati, quod beatae,
-        deleniti dicta error hic veniam qui. Eveniet praesentium minus, voluptas
-        excepturi aliquid ut nobis necessitatibus sed dolorum consectetur nemo
-        nulla nam rerum quas cum et possimus suscipit, alias rem quae nostrum.
-        Est ratione in illum ducimus, nostrum, deleniti, iste eveniet placeat
-        repellendus voluptas repudiandae quam sequi natus doloremque odit
-        dolorem saepe nobis qui distinctio consectetur. Mollitia alias quasi, a
-        quos nam corrupti magnam? Voluptatum vero vitae voluptate repudiandae
-        ratione hic, quisquam est veniam architecto dolore possimus, harum
-        veritatis ex. Minus soluta dolor alias vitae repudiandae quas esse sint,
-        doloribus doloremque fugiat harum eum, obcaecati facilis molestiae id
-        explicabo sunt iure quisquam quos possimus. Voluptatibus nemo ab maiores
-        hic iure blanditiis a quis deleniti error voluptas debitis, at molestiae
-        neque! Enim quos tempora placeat dolor? A neque autem facilis quis
-        distinctio dolorum quas itaque quibusdam in. Cupiditate, rem provident?
-        Sint rem in animi facilis deleniti quae, omnis atque dolorem cum est,
-        similique placeat aliquam praesentium eum. Consectetur molestiae esse
-        ullam ex, quisquam deleniti omnis necessitatibus quam nam reprehenderit
-        cum perspiciatis nihil. Amet illo quam, minima dicta corrupti atque
-        repellat dolores nemo voluptate ut est itaque, repellendus ad magnam?
-        Amet neque voluptates sit omnis nemo repudiandae quia atque cupiditate
-        et temporibus! Blanditiis numquam doloremque iste culpa recusandae,
-        necessitatibus excepturi provident sit voluptate accusantium mollitia
-        reprehenderit, hic porro nulla vero? Sapiente dolore voluptas doloribus
-        cum, necessitatibus rerum voluptatibus provident et earum quam dolores
-        maiores tempore? Sint possimus neque ex cum quod obcaecati aliquid,
-        minima, autem velit itaque ut hic accusantium magni ipsam omnis
-        aspernatur odio consectetur quia. Voluptas, id magni laudantium debitis
-        excepturi doloribus ratione in sunt aperiam sint non illo quo
-        repudiandae, earum ex hic recusandae minima labore vitae aut architecto!
-        Aliquam ad eveniet sunt blanditiis deserunt, velit sequi error? Quo,
-        voluptatem quae, quibusdam, corporis laboriosam iste ducimus doloremque
-        natus vel repudiandae aliquid!
+      <div className=" bg-[#F9F9F9] w-full  text-justify pl-[80px] large:ml-0 ">
+        <div className="flex justify-between items-center p-4">
+          <button className="btn btn-ghost text-xl text-[#646F75]">Home</button>
+
+          {/* toggle and profile*/}
+          <div className=" flex justify-center item-center gap-3">
+          <div className=" flex justify-center items-center">
+            <label className="cursor-pointer grid place-items-center">
+              <input
+                type="checkbox"
+                value="synthwave"
+                className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+              />
+              <svg
+                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <svg
+                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
+          </div>
+          <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+        </div>
       </div>
     </div>
   );
